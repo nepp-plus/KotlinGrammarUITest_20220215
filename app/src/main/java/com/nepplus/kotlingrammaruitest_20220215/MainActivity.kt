e55faa92756ad13f8dcf0a0048b389f9d081f8f3
nepp.plus <nepp.plus@gmail.com>
2022-02-15 15:55:29 +0900

    로그인 성공인 경우 처리

--- app/src/main/java/com/nepplus/kotlingrammaruitest_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlingrammaruitest_20220215/MainActivity.kt
@@ -2,10 +2,26 @@ package com.nepplus.kotlingrammaruitest_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+
+            val inputId = edtId.text.toString()
+            val inputPassword = edtPassword.text.toString()
+
+            if ( inputId == "admin" && inputPassword == "asdf" ) {
+
+                Toast.makeText(this, "관리자님 환영합니다.", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
     }
 }
